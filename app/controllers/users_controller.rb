0ca953a34f8055fefc1a25964dba40ca4679b66d
Edgardo Marti <emarti@gmail.com>
2024-06-16 03:56:15 -0300

    Added gpg_key generation for users and functional views.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -58,10 +58,40 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /users/1/gpg_show
+  # GET /users/gpg_show/:id
   def gpg_show
     @user = User.find(params[:id])
     @page_libs = [:openpgp, :pgpKeyGenerate]
+  end
+
+  # POST /users/gpg_update/:id
+  def gpg_update
+    @user = User.find(params[:id])
+    @user.gpg_key = GpgKey.build(description: "rsa4096", gpg_public_key: params[:pubKey])
+ 
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to user_url(@user), notice: "GPG Public Key was successfully updated." }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /users/gpg_destroy/:id
+  def gpg_destroy
+    @user = User.find(params[:id])
+    @user.gpg_key=nil
+    @user.save
+    #userKey=GpgKey.find_by_id(User.find_by(id:4).gpg_key_id)
+    #userKey.destroy!
+
+    respond_to do |format|
+      format.html { redirect_to users_gpg_show_url(@user), notice: "GPG Public Key was successfully destroyed." }
+      format.json { head :no_content }
+    end
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :teams
   resources :credentials
   resources :users
-  get 'users/gpg_show/:id' => 'users#gpg_show'
+  get 'users/gpg_show/:id' => 'users#gpg_show', as: :users_gpg_show
+  post 'users/gpg_update/:id' => 'users#gpg_update', as: :users_gpg_update
+  delete 'users/gpg_destroy/:id' => 'users#gpg_destroy', as: :users_gpg_destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
