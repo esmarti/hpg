e64cb6b7a32a6dd4da8d8123bf251537c663545a
Edgardo Marti <emarti@gmail.com>
2024-07-14 19:36:36 -0300

    Changed GPG Key type in generation keypair for users, because RSA 4096bits takes too much time and hangs the browser. Instead of, we choose ECC 256bits.

--- app/assets/javascripts/pgpKeyGenerate.js
+++ app/assets/javascripts/pgpKeyGenerate.js
@@ -1,20 +1,39 @@
+// async function pgpGenerateKey(userID, email, passphrase) {
+//     const keyPair = await openpgp.generateKey({
+//         type: 'rsa', // Type of the key
+//         rsaBits: 4096, // RSA key size (defaults to 4096 bits)
+//         userIDs: [{ name: userID, email: email }], // you can pass multiple user IDs
+//         passphrase: passphrase // protects the private key
+//     });
+
+//     const privateKey = keyPair.privateKey;
+//     const publicKey = keyPair.publicKey;
+
+//     //Mostrar la key en un campo de la vista
+//     document.getElementById("pubKey").value=keyPair.publicKey;
+//     document.getElementById("privKey").value=keyPair.privateKey;
+
+//     return;
+// }
+
 async function pgpGenerateKey(userID, email, passphrase) {
-    const keyPair = await openpgp.generateKey({
-        type: 'rsa', // Type of the key
-        rsaBits: 4096, // RSA key size (defaults to 4096 bits)
+    const { privateKey, publicKey, revocationCertificate } = await openpgp.generateKey({
+        type: 'ecc', // Type of the key, defaults to ECC
+        curve: 'curve25519', // ECC curve name, defaults to curve25519 - 256 bits key
         userIDs: [{ name: userID, email: email }], // you can pass multiple user IDs
-        passphrase: passphrase // protects the private key
+        passphrase: passphrase, // protects the private key
+        format: 'armored' // output key format, defaults to 'armored' (other options: 'binary' or 'object')
     });
 
-    const privateKey = keyPair.privateKey;
-    const publicKey = keyPair.publicKey;
-
+    //console.log(revocationCertificate); // '-----BEGIN PGP PUBLIC KEY BLOCK ... '
+    
     //Mostrar la key en un campo de la vista
-    document.getElementById("pubKey").value=keyPair.publicKey;
-    document.getElementById("privKey").value=keyPair.privateKey;
+    document.getElementById("pubKey").value=publicKey;
+    document.getElementById("privKey").value=privateKey;
 
     return;
 }
+
 
 function copyToClipboard(elementId) {
     const copyText = document.getElementById(elementId);
