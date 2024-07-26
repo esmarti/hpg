f96740fc23f075d6f88db2b26eb6aafef8e63589
Edgardo Marti <emarti@gmail.com>
2024-07-26 00:21:09 -0300

    Added a “Dashboard” controller for the “admin” role. Here will be the access to the most common tasks for the platform administrator. Added a public “Pages” controller for all users. It serves as the home page (root_path) for the web site and project identification (about). Fixed the sign_out button in the navigation bar that was not working.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
+
   # Routers for Devise.
   # The 'users/registrations' controller is for curstomizations in the signup form.
   devise_for :users, :controllers => { registrations: 'users/registrations' }
-
-  namespace :admin do
-    resources :users, :teams
-  end
-
+  
   resources :users, only: [:new, :index, :edit, :show, :update, :gpg_show, :gpg_update, :gpg_destroy] do
     resources :credentials
     resources :teams
@@ -15,12 +12,20 @@ Rails.application.routes.draw do
   get 'users/gpg_show/:id' => 'users#gpg_show', as: :users_gpg_show
   post 'users/gpg_update/:id' => 'users#gpg_update', as: :users_gpg_update
   delete 'users/gpg_destroy/:id' => 'users#gpg_destroy', as: :users_gpg_destroy
+
+  namespace :admin do
+    resources :users, :teams
+    get 'dashboard' => 'dashboard#index', as: :dashboard
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # Define public routes
+  get 'about', to: 'pages#about'
   # Defines the root path route ("/")
-  root "users#index"
+  root 'pages#home'
 end
