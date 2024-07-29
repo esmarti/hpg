2b4d35ef6562d128cffb6c00f172094480b2276e
Edgardo Marti <emarti@gmail.com>
2024-07-29 01:23:27 -0300

    Added content to static pages home and about.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
   def home
+    #this page is the principal dashboard when the user is authenticated
   end
 
   def about
+    #this page is the about public page
+
   end
 end
