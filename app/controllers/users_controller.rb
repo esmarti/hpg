550ef8aad075516739ae1af4211a20fa0b6e8faa
Edgardo Marti <emarti@gmail.com>
2024-11-05 01:44:43 -0300

    Fixed bug in users controller. Now you can call users/pgp_show/:id to see user keys.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: %i[ edit update destroy ]
+  before_action :set_user, only: %i[ edit update ]
 
   # GET /users/1/edit
   def edit
